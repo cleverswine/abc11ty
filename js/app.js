c4eca2edbe51493ab64f2c3f7b66410f6ef809dd
Kevin Noone <kevin.noone@surescripts.com>
2025-01-09 12:54:07 -0600

    tabs

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const tabButtons = document.querySelectorAll('.section-button');
+const tabContents = document.querySelectorAll('.tabs__panels > div');
+
+if (tabButtons && tabContents) {
+    tabButtons.forEach((tabBtn) => {
+        tabBtn.addEventListener('click', () => {
+            const tabId = tabBtn.getAttribute('data-id');
+
+            tabButtons.forEach((btn) => btn.classList.remove('active'));
+            tabBtn.classList.add('active');
+
+            tabContents.forEach((content) => {
+                content.classList.remove('active');
+                if (content.classList.contains(tabId)) {
+                    content.classList.add('active');
+                }
+            });
+        });
+    });
+}
