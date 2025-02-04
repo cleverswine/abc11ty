c2d44e4b9579f88f0b4664786993e6f6a620db5d
cleverswine <cleverswine@gmail.com>
2025-02-04 13:20:57 -0600

    bootstrap

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 module.exports = function (eleventyConfig) {
-  // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
+    // Copy `img/` to `_site/img`
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
 
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+
+    // eleventyConfig.addPassthroughCopy({"node_modules/bootstrap/dist/css/bootstrap.min.css": "css/bootstrap.min.css"});
+    // eleventyConfig.addPassthroughCopy({"node_modules/bootstrap/dist/js/bootstrap.min.js": "js/bootstrap.min.js"});
 };
